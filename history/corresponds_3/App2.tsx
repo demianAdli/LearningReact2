66b4ae9bc2434d984baa2465cc53dbe417bf8772
Alireza Adli <alireza.adli@mail.concordia.ca>
2023-10-28 11:00:04 -0400

    Add folders from the previous Learning React

---
+++ history/corresponds_3/App2.tsx
@@ -0,0 +1,21 @@
+/*
+import Alert from "./components/Alert";
+import Button from "./components/Button";
+import { useState } from "react";
+
+function App() {
+  const [alertVisible, setAlertVisibility] = useState(false);
+  return (
+    <div>
+      {alertVisible && (
+        <Alert onClose={() => setAlertVisibility(false)}>My alert</Alert>
+      )}
+      <Button color="primary" onClick={() => setAlertVisibility(true)}>
+        My Button
+      </Button>
+    </div>
+  );
+}
+
+export default App;
+*/
---
+++ src/App.tsx
@@ -0,0 +1,25 @@
+import { useState } from "react";
+import ExpandableText from "./components/ExpandableText.tsx";
+
+function App() {
+  return (
+    <div>
+      <ExpandableText>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis
+        accusamus ex dicta veniam consequatur modi sit nisi corrupti non dolorum
+        excepturi, consectetur laudantium neque exercitationem, ad a corporis!
+        Nemo temporibus veniam, exercitationem excepturi reprehenderit modi
+        impedit nam consequatur eius molestiae velit quos sunt molestias dicta.
+        Veritatis voluptas modi officiis accusantium quaerat error, nulla, quae
+        ab, commodi ad voluptate eveniet voluptates rerum quidem saepe minima
+        repellat quam. Doloremque, magnam adipisci dolor laborum quia, numquam
+        voluptates id culpa accusamus voluptatem dolores excepturi dicta facilis
+        ratione autem aspernatur ducimus ullam! Reiciendis dolorum doloribus
+        laborum cumque nulla quibusdam, deleniti eum, beatae et voluptate
+        tempora!
+      </ExpandableText>
+    </div>
+  );
+}
+
+export default App;
